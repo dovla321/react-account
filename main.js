a9babdfcaa79ea03d9a0e6f8f36f913f69122364
dovla321 <dovla91mos@gmail.com>
2022-06-19 18:29:35 -1200

    zavrišili smo i edit btn funkcionalnost

--- main.js
+++ main.js
@@ -12,7 +12,34 @@ let emailInput = document.querySelector('[placeholder="email"]');
 let phoneInput = document.querySelector('[placeholder="phone"]');
 let saveBtn = document.querySelector('#save');
 
+// SELEKTOVANJE EDIT ACCOUNT VIEW KLASA
+
+let eId = document.querySelector('.eId');
+let eName = document.querySelector('.eName');
+let eLastName = document.querySelector('.eLastName');
+let eEmail = document.querySelector('.eEmail');
+let ePhone = document.querySelector('.ePhone');
+let editBtn = document.querySelector('#edit');
+let id;
+
+editBtn.addEventListener('click',saveEditedAccount);
 saveBtn.addEventListener('click', saveAccount);
+
+
+
+function saveEditedAccount(){
+	const editedAccount = {
+		id:eId.value,
+		name:eName.value,
+		lastname:eLastName.value,
+		email:eEmail.value,
+		phone:ePhone.value
+	}
+
+	db[id] = editedAccount;
+	createAccountsTable();
+	showView("#accounts-view");
+}
 
 function saveAccount(){
 	const newAccount = {
@@ -97,5 +124,12 @@ function deleteAccount(){
 }
 
 function editAccount(){
-	
+	 id = this.getAttribute('data-id');
+	let selectedAccount = db[id];
+	eId.value = selectedAccount.id;
+	eName.value = selectedAccount.name;
+	eLastName.value = selectedAccount.lastname;
+	eEmail.value = selectedAccount.email;
+	ePhone.value = selectedAccount.phone;
+	showView("#edit-account-view");
 }
