9d3217f0fd151d03c068f7c36886d8de5e2bc588
dovla321 <dovla91mos@gmail.com>
2022-06-19 17:00:08 -1200

    napravili smo funkcionalno input polje za novog account koeisnika

--- main.js
+++ main.js
@@ -4,19 +4,59 @@ let addAccountsView = document.querySelector('#add-account-view');
 let allLinks = document.querySelectorAll('.nav-link');
 let views = document.querySelectorAll('.view');
 
+// SELEKTOVANJE INPUT POLJA POMOĆU PLACEHOLDERA
+let idInput = document.querySelector('[placeholder="id"]');
+let nameInput = document.querySelector('[placeholder="name"]');
+let lastNameInput = document.querySelector('[placeholder="lastname"]');
+let emailInput = document.querySelector('[placeholder="email"]');
+let phoneInput = document.querySelector('[placeholder="phone"]');
+let saveBtn = document.querySelector('#save');
+
+saveBtn.addEventListener('click', saveAccount);
+
+function saveAccount(){
+	const newAccount = {
+		id: idInput.value,
+		name: nameInput.value,
+		lastname: lastNameInput.value,
+		email: emailInput.value,
+		phone :phoneInput.value
+	}
+
+	db.push(newAccount);
+	idInput.value = "";
+	nameInput.value = "";
+	lastNameInput.value = "";
+	emailInput.value = "";
+	phoneInput.value = "";
+	createAccountsTable();
+	showView("#accounts-view");
+}
+
+
+
+
+
 
 for(let i = 0; i < allLinks.length; i++){
 allLinks[i].addEventListener('click', showView);
 }
 
 function showView(e){
-	e.preventDefault();
+
 	for(let i = 0; i < views.length; i++){
 		views[i].style.display = "none";
 	}
 
-	let id = `#${this.getAttribute("href")}`;
-	document.querySelector(id).style.display = "block";
+
+	if(e instanceof Event){
+		e.preventDefault();
+		let id = `#${this.getAttribute("href")}`;
+		document.querySelector(id).style.display = "block";
+	}else{
+		document.querySelector(e).style.display = "block";
+	}
+	
 	
 }
 
